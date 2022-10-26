f213ddf067b11060b792e8b5d6946de773b2dc59
andriibeha <beha.andrii@gmail.com>
2022-10-26 11:04:25 +0300

    HW Done

--- models/contact.js
+++ models/contact.js
@@ -2,30 +2,33 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 const { handleSaveError } = require("../middelwares");
 
-const contactSchema = new Schema({
+const contactSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'Set name for contact'],
+      type: String,
+      required: [true, "Set name for contact"],
     },
     email: {
-        type: String,
+      type: String,
     },
     phone: {
-        type: String,
+      type: String,
     },
     favorite: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
-});
+  },
+  { versionKey: false }
+);
 
 contactSchema.post("save", handleSaveError);
 
 const addSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string(),
-    phone: Joi.string(),
-    favorite: Joi.boolean(),
+  name: Joi.string().required(),
+  email: Joi.string(),
+  phone: Joi.string(),
+  favorite: Joi.boolean(),
 });
 
 const updateScema = Joi.object({
@@ -36,18 +39,18 @@ const updateScema = Joi.object({
 }).min(1);
 
 const updateFavoriteShema = Joi.object({
-    favorite: Joi.boolean().required(),
-})
+  favorite: Joi.boolean().required(),
+});
 
 const schemas = {
-    addSchema,
-    updateScema,
-    updateFavoriteShema,
+  addSchema,
+  updateScema,
+  updateFavoriteShema,
 };
 
-const Contact = model('contact', contactSchema);
+const Contact = model("contact", contactSchema);
 
 module.exports = {
-    Contact, 
-    schemas,
+  Contact,
+  schemas,
 };
